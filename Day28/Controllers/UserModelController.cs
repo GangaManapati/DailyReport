fcd9dc69d569eeb32bd060b9d920fc44ceb580c1
GangaManapati <167989286+GangaManapati@users.noreply.github.com>
2024-05-28 14:35:24 +0530

    Add files via upload

---
+++ Day28/Controllers/UserModelController.cs
@@ -0,0 +1,26 @@
+﻿using Day2852024.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day2852024.Controllers
+{
+    public class UserModelController : Controller
+    {
+        [HttpPost]
+        public ActionResult Register(UserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save the data to the database
+                return RedirectToAction("Register");
+            }
+
+            // If we got this far, something failed; redisplay form
+            return View(model);
+        }
+
+    }
+}
