31987a8ebff5972e24dc00c7935c71d4d77a25da
GangaManapati <167989286+GangaManapati@users.noreply.github.com>
2024-06-04 18:35:04 +0530

    Add files via upload

---
+++ Day4624/Controllers/CrudApiController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication4624.Models;
+
+namespace WebApplication4624.Controllers
+{
+    public class CrudApiController : ApiController
+    {
+        web_api_CRUD_dbEntities1 db=new web_api_CRUD_dbEntities1();
+        [HttpGet]
+        public IHttpActionResult GetEmployee()
+        {
+            List<Employee> list = db.Employees.ToList();
+            return Ok(list);
+        }
+        [HttpGet]
+        public IHttpActionResult GetEmployeIde(int id)
+        {
+            Employee e=db.Employees.Where(c=>c.id==id).SingleOrDefault();  
+            return Ok(e);
+        }
+         [HttpPost]
+        public IHttpActionResult Emp(Employee e)
+        {
+            db.Employees.Add(e);
+            db.SaveChanges();
+            return Ok();
+        }
+
+
+        [HttpPut]
+        public IHttpActionResult EmpUpdate(Employee e)
+        {
+            db.Entry(e).State=System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+
+            /* Employee s = db.Employees.Where(c => c.id == e.id).SingleOrDefault();
+             if (s != null)
+             {
+                   s.id = e.id;
+                 s.name = e.name;
+                 s.age = e.age;
+                 s.gender = e.gender;
+                 s.salary = e.salary;
+                 s.designition = e.designition;
+                 db.SaveChanges();
+
+             }
+             else
+             {
+                 return NotFound();
+             } 
+
+
+             return Ok(s);
+            */
+
+            return Ok();
+        }
+        [HttpDelete]
+        public IHttpActionResult EmpDelete(int id)
+        {
+            Employee e = db.Employees.Where(c => c.id == id).SingleOrDefault();
+            db.Entry(e).State = System.Data.Entity.EntityState.Deleted;
+            db.SaveChanges();
+            return Ok();
+        }
+
+
+    }
+}
---
+++ Day4624/Controllers/CrudMvcController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure.MappingViews;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+using WebApplication4624.Models;
+
+namespace WebApplication4624.Controllers
+{
+    public class CrudMvcController : Controller
+    {
+        // GET: CrudMvc
+        HttpClient cli = new HttpClient();
+        public ActionResult Index()
+        {
+            List<Employee> emp_list = new List<Employee>();
+            cli.BaseAddress = new Uri("http://localhost:59098/api/CrudApi");
+            var x = cli.GetAsync("CrudApi");  // for getting the data
+            x.Wait();
+            var test = x.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var dis = test.Content.ReadAsAsync<List<Employee>>();
+                dis.Wait();
+                emp_list = dis.Result;
+            }
+            return View(emp_list);
+
+
+        }
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            Employee p = null;
+            cli.BaseAddress = new Uri("http://localhost:59098/api/CrudApi");
+            var x = cli.GetAsync("CrudApi?id=" + id.ToString());  // for getting the data
+            x.Wait();
+            var test = x.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var dis = test.Content.ReadAsAsync<Employee>();
+                dis.Wait();
+                p = dis.Result;
+            }
+            return View(p);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee emp)
+        {
+            cli.BaseAddress = new Uri("http://localhost:59098/api/CrudApi");
+            var x = cli.PostAsJsonAsync("CrudApi", emp);   // for post the data
+            x.Wait();
+            var t = x.Result;
+            if (t.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Create");
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            Employee p = null;
+            cli.BaseAddress = new Uri("http://localhost:59098/api/CrudApi");
+            var x = cli.GetAsync("CrudApi?id=" + id.ToString());  // for getting the data
+            x.Wait();
+            var test = x.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var dis = test.Content.ReadAsAsync<Employee>();
+                dis.Wait();
+                p = dis.Result;
+            }
+            return View(p);
+
+
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Employee e)
+        {
+
+            cli.BaseAddress = new Uri("http://localhost:59098/api/CrudApi");
+            var x = cli.PutAsJsonAsync<Employee>("CrudApi", e);   // for post the data
+            x.Wait();
+            var t = x.Result;
+            if (t.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Edit");
+
+        }
+        public ActionResult Delete(int id)
+        {
+            Employee p = null;
+            cli.BaseAddress = new Uri("http://localhost:59098/api/CrudApi");
+            var x = cli.GetAsync("CrudApi?id=" + id.ToString());  // for getting the data
+            x.Wait();
+            var test = x.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var dis = test.Content.ReadAsAsync<Employee>();
+                dis.Wait();
+                p = dis.Result;
+            }
+            return View(p);
+
+
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirm(int id)
+        {
+            cli.BaseAddress = new Uri("http://localhost:59098/api/CrudApi");
+            var x = cli.DeleteAsync("CrudApi/"+id.ToString());   // for post the data
+            x.Wait();
+            var t = x.Result;
+            if (t.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Delete");
+        }
+    }
+}
