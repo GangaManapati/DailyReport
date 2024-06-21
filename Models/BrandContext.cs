fccb28ef3d359aa632057ed5bfd6546b74d4a771
GangaManapati <167989286+GangaManapati@users.noreply.github.com>
2024-06-21 18:20:09 +0530

    Add files via upload

---
+++ Models/BrandContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication210624.Models
+{
+    public class BrandContext :DbContext
+    {
+        public BrandContext(DbContextOptions<BrandContext> Options) :base(Options) 
+        {
+        
+        }  
+        public DbSet<Brand> Brands { get; set; }
+            
+        
+    }
+}
