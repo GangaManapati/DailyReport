292ad594b143f51ea8b038e1417140ff4b0dc706
GangaManapati <167989286+GangaManapati@users.noreply.github.com>
2024-06-22 18:10:15 +0530

    Add files via upload

---
+++ Jwt/JwtImplementatio/JwtImplementatio/Controllers/LoginController.cs
@@ -0,0 +1,70 @@
+﻿using JwtImplementatio.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JwtImplementatio.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        private User AuthenticatUser(User user)
+        {
+            User _user = null;
+            if (user.UserName == "Admin" && user.Password == "12345")
+            {
+                _user = new User
+                {
+                    UserName = "Ganga"
+                };
+            }
+            return _user;
+        }
+        private string GenerateToken(User user)
+        {
+            // var SymmetricKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login( User user)
+        {
+            IActionResult response = Unauthorized();
+            var authenticatedUser = AuthenticatUser(user);
+            if (authenticatedUser != null)
+            {
+                var token = GenerateToken(authenticatedUser);
+                return Ok(new { token=token });
+            }
+            return response;
+        }
+
+    }
+}
