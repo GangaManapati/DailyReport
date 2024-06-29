1c9bc7e14e7e25fa1f29a3006f4b56bdee8877f1
GangaManapati <167989286+GangaManapati@users.noreply.github.com>
2024-06-29 18:24:27 +0530

    Add files via upload

---
+++ Depency/DependencyInJectionC/DependencyInJectionC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DependencyInJectionC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
